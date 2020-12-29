0196cef5b4a1cd34337dcec92f8bce9a44ef528d
Vikas Singh <vandevikas@gmail.com>
2020-12-29 21:25:43 +0530

    Added Articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController 
     def show
-        @article = Article.find(params[:id]) #params hash to pass id from site
+        @article = Article.find(params[:id]) #params hash
+    end
+    
+    def index
+        @articles = Article.all
     end
 end
