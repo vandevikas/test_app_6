e62aba00197c92098f1f86f048212ffd7e1d8dbf
Vikas Singh <vandevikas@gmail.com>
2020-12-30 18:48:42 +0530

    Added Delete and Show functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,12 +26,18 @@ class ArticlesController < ApplicationController
     end
 
    def update
-        @article = Article.find(params[:id])
+         
         if @article.update(params.require(:article).permit(:title, :description))
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
             render 'edit'
         end
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end 
 end
