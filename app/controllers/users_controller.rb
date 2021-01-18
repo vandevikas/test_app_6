b6b9a1d9c206409f645eeba70f75c06037d51f2f
Vikas Singh <vandevikas@gmail.com>
2021-01-18 14:03:24 +0530

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id ])
+    @user = User.find(params[:id])
   end
 
   def update
