286c9004c4b4c6c18f188befe87591b06d500d98
Vikas Singh <vandevikas@gmail.com>
2021-01-20 10:20:50 +0530

    login of user with sessioins

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
-  #post 'users', to: 'users#create'
   resources :users, except: [:new] 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
 end
